6687e36ea712cb78c83bbbd510aae8f33d84d9b9
senuch <s.uzairtariq@gmail.com>
2023-06-28 19:48:26 +0500

    Boundry Protection for Circle

--- axe_game.cpp
+++ axe_game.cpp
@@ -20,12 +20,12 @@ int main()
             // Game logic begins
             DrawCircle(circle_x, circle_y, 25, BLUE);
 
-            if(IsKeyDown(KEY_D))
+            if(IsKeyDown(KEY_D) && circle_x < width)
             {
                   circle_x = circle_x + 10;
             }
 
-            if(IsKeyDown(KEY_A))
+            if(IsKeyDown(KEY_A) && circle_x > 0)
             {
                   circle_x = circle_x - 10;
             }
