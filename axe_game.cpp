01118c7fc2309fbe2daef53a90b01599686bdb98
senuch <s.uzairtariq@gmail.com>
2023-06-28 19:45:56 +0500

    Added Input and Rendered Circle

--- axe_game.cpp
+++ axe_game.cpp
@@ -2,5 +2,35 @@
 
 int main()
 {
-      InitWindow(350, 200, "Jerry");
+      // window dimensions
+      int width{350};
+      int height{200};
+      InitWindow(width, height, "Jerry");
+
+      // circle coordinates
+      int circle_x{175};
+      int circle_y{100};
+
+      SetTargetFPS(60);
+      while (!WindowShouldClose())
+      {
+            BeginDrawing();
+            ClearBackground(WHITE);
+
+            // Game logic begins
+            DrawCircle(circle_x, circle_y, 25, BLUE);
+
+            if(IsKeyDown(KEY_D))
+            {
+                  circle_x = circle_x + 10;
+            }
+
+            if(IsKeyDown(KEY_A))
+            {
+                  circle_x = circle_x - 10;
+            }
+
+            // Game logic ends
+            EndDrawing();
+      }
 }
